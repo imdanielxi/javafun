ae9aaaf4c8b9375aa1037f7f5be164c62e846e0b
danielxi <daniel.xi.1997@gmail.com>
2016-06-12 18:36:21 -0700

    findAllPairs done

--- src/findAllPairs.java
+++ src/findAllPairs.java
@@ -4,8 +4,30 @@
 public class findAllPairs {
 
     //create a "hello world" program that prints out "hello world"
+    //now given a sorted array of integers, create a method to find all unique pairs taht add up to a certain number
     public static void main(String[] args) {
         System.out.println("hello world");
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        int sum = 10;
+        findAllPairs obj = new findAllPairs();
+        obj.findPairs(arr, sum);
     }
 
+    public void findPairs(int[] arr, int sum){
+        for(int i = 0;i < arr.length; i++){
+            int x = arr[i];
+            for(int j = i + 1; j <arr.length; j++){
+                int y = arr[j];
+
+                if((x + y)== sum){
+                    String toPrint = arr[i]+" | "+arr[j];
+                    System.out.println(toPrint);
+                }
+
+            }
+        }
+    }
+
+
 }
+
