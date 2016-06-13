7a8f442d5e08571240c850c8d6fdbf3724f08489
danielxi <daniel.xi.1997@gmail.com>
2016-06-12 17:49:39 -0700

    prints helloworld

---
+++ src/findAllPairs.java
@@ -0,0 +1,11 @@
+/**
+ * Created by danielxi on 6/12/16.
+ */
+public class findAllPairs {
+
+    //create a "hello world" program that prints out "hello world"
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
